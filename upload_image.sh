8ed19732964579241252d7ceb77ece285d70c723
up201605219 <up201605219@fe.up.pt>
2019-03-07 10:58:35 +0000

    user profile page almost done

--- upload_image.sh
+++ upload_image.sh
@@ -3,8 +3,8 @@
 # Stop execution if a step fails
 set -e
 
-DOCKER_USERNAME=joaoccm # Replace by your docker hub username
-IMAGE_NAME=lbaw1863
+DOCKER_USERNAME=lbaw1863 # Replace by your docker hub username
+IMAGE_NAME=lbaw1863-piu
 
 docker build -t $DOCKER_USERNAME/$IMAGE_NAME .
 docker push $DOCKER_USERNAME/$IMAGE_NAME
