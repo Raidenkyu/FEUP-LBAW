deec691195cd0817ea1eefe0763c45da9742fc1b
FernandoJAlves <up201605270@fe.up.pt>
2019-03-13 00:13:26 +0000

    Added settings page

--- html/js/settings.js
+++ html/js/settings.js
@@ -1,12 +1,13 @@
 let sidebar = document.querySelector("#mySidenav");
+let size = "320px";
 
 function settingsButtonClicked() {
-    if(sidebar.style.width == "250px"){
+    if(sidebar.style.width == size){
         sidebar.style.width = "0";
     }
     else{
         
-        sidebar.style.width = "250px";
+        sidebar.style.width = size;
     }
   }
   
