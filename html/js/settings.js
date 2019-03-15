a444280fa280f30b20f2977c8f38d90f962d45f7
FernandoJAlves <up201605270@fe.up.pt>
2019-03-15 10:27:43 +0000

    Added grey-out effect to settings

--- html/js/settings.js
+++ html/js/settings.js
@@ -4,8 +4,14 @@ let negSize = "-320px";
 function settingsButtonClicked() {
     if(sidebar.style.right == negSize || sidebar.style.right == ""){
         sidebar.style.right = "0";
+        document.getElementById('opaque').style.display='block';
     }
     else{
         sidebar.style.right = negSize;
+        document.getElementById('opaque').style.display='none';
     }
+    
   }
+
+
+
