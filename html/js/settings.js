7f596f4ab766bd3e276d5f03f5153ca46124b33f
up201605219 <up201605219@fe.up.pt>
2019-03-18 12:21:05 +0000

    Inline JavaScript removed

--- html/js/settings.js
+++ html/js/settings.js
@@ -1,17 +1,30 @@
 let sidebar = document.querySelector("#mySidenav");
 let negSize = "-320px";
+let settingsButton = document.querySelector("#sidebarCollapse");
+let opaque = document.getElementById('opaque');
+
+
+settingsButton.addEventListener('click', settingsButtonClicked);
+opaque.addEventListener('click', closeSettings);
 
 function settingsButtonClicked() {
-    if(sidebar.style.right == negSize || sidebar.style.right == ""){
+    if (sidebar.style.right == negSize || sidebar.style.right == "") {
         sidebar.style.right = "0";
-        document.getElementById('opaque').style.display='block';
+        opaque.style.display = 'block';
     }
-    else{
+    else {
         sidebar.style.right = negSize;
-        document.getElementById('opaque').style.display='none';
+        opaque.style.display = 'none';
     }
-    
-  }
+
+}
+
+function closeSettings(event) {
+    if (opaque.style.display == 'block') {
+        sidebar.style.right = negSize;
+        opaque.style.display = 'none';
+    }
+}
 
 
 
