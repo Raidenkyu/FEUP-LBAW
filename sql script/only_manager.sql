96e6572198b5c7d71e795cad142caa7cfd4eb967
Cláudio Lemos <cflemos1999@me.com>
2019-04-09 12:44:37 +0100

    New trigger

---
+++ sql script/only_manager.sql
@@ -0,0 +1,10 @@
+DROP TRIGGER IF EXISTS OnlyManager;
+
+CREATE TRIGGER IF NOT EXISTS OnlyManager
+BEFORE DELETE ON project_member
+BEGIN
+  IF (SELECT COUNT(*) FROM(SELECT id_member FROM project_member WHERE id_project = OLD.id_project AND manager = true) AS id) = 1
+  THEN
+    RAISE 'Cannot remove only manager'
+  ENDIF;
+END;
