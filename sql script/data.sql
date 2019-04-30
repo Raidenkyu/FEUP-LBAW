bed6594a4ac22446ce838876a17d4268fe8025fa
Fernando Jorge Coelho Barreira Calheiros Alves <up201605270@fe.up.pt>
2019-04-30 12:23:17 +0100

    Update data.sql

--- sql script/data.sql
+++ sql script/data.sql
@@ -68,7 +68,7 @@ INSERT INTO google_auth (id_member, password) VALUES(20, '03AC674216F3E15C761EE1
 
 -- project (id_project, name, color, end_date, deleted)
 INSERT INTO project (id_project, name, color, end_date, deleted) VALUES(1, 'workpad', 'Green', '2019-06-02 12:00:00', false);
-INSERT INTO project (id_project, name, color, end_date, deleted) VALUES(2, 'NIFEUP', 'Orange', '2019-06-02 12:00:00', false);
+INSERT INTO project (id_project, name, color, end_date, deleted) VALUES(2, 'NIAEFEUP', 'Orange', '2019-06-02 12:00:00', false);
 INSERT INTO project (id_project, name, color, end_date, deleted) VALUES(3, 'IEEE', 'Blue', '2019-06-02 12:00:00', false);
 INSERT INTO project (id_project, name, color, end_date, deleted) VALUES(4, 'AEFEUP', 'Yellow', '2019-06-02 12:00:00', false);
 INSERT INTO project (id_project, name, color, end_date, deleted) VALUES(5, 'Tuga POP', 'Red', '2019-06-02 12:00:00', false);
