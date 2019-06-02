0c84640c1a6a6b22b05472f389cc70cd8272d3ee
Pedro Gonçalves <pedrokraktuga@gmail.com>
2019-06-02 19:50:48 +0100

    Project settings is now dynamic

--- app/Http/Controllers/ProjectSettingsController.php
+++ app/Http/Controllers/ProjectSettingsController.php
@@ -14,5 +14,59 @@ class ProjectSettingsController extends Controller
     }
 
     public function update($id){
+        $project = \App\Project::where('id_project', $id)->first();
+        request()->validate([
+            'name' => 'required | min:3',
+            'color' => 'required'
+        ]);
+
+        $project->name = request('name');
+        $project->color = $this->colorPicker(request('color'));
+        $project->save();
+        return $project;
+    }
+
+    private function colorPicker($color) {
+        switch ($color) {
+            case 'color-1':
+                return 'Orange';
+
+            case 'color-2':
+                return 'Yellow';
+
+            case 'color-3':
+                return 'Red';
+
+            case 'color-4':
+                return 'Green';
+
+            case 'color-5':
+                return 'Lilac';
+
+            case 'color-6':
+                return 'Sky';
+
+            case 'color-7':
+                return 'Brown';
+
+            case 'color-8':
+                return 'Golden';
+
+            case 'color-9':
+                return 'Bordeaux';
+
+            case 'color-10':
+                return 'Emerald';
+
+            case 'color-11':
+                return 'Purple';
+
+            case 'color-12':
+                return 'Blue';
+            
+            default:
+                return 'Orange';
+                break;
+        }
     }
 }
