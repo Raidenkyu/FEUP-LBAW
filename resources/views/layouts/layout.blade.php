b05e63492a6bdbdc771558d46c2c223877b76414
Pedro Gonçalves <pedrokraktuga@gmail.com>
2019-06-05 14:09:22 +0100

    Fixed missing aspa

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -6,7 +6,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <link rel="stylesheet href="{{ asset('/css/roboto.css') }}">
+  <link rel="stylesheet" href="{{ asset('/css/roboto.css') }}">
   <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
   <link rel="stylesheet" href="{{ asset('/css/index.css') }}">
   <link rel="stylesheet" href="{{ asset('/css/footer.css') }}">
