1822cbb84cfd4d7bef7799536eb45f3cf9031262
Pedro Gonçalves <pedrokraktuga@gmail.com>
2019-06-06 23:58:30 +0100

    Fixed HTML Errors for Validator

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -6,19 +6,15 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <link href="https://fonts.googleapis.com/css?family=Noto+Sans+TC|Roboto:100,200,300,400,500,600,700,800,900"
+  <link href="https://fonts.googleapis.com/css?family=Noto+Sans+TC%7CRoboto:100,200,300,400,500,600,700,800,900"
     rel="stylesheet">
   <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
   <link rel="stylesheet" href="{{ asset('/css/index.css') }}">
   <link rel="stylesheet" href="{{ asset('/css/footer.css') }}">
-  <script type="text/javascript">
-    // Fix for Firefox autofocus CSS bug
-    // See: http://stackoverflow.com/questions/18943276/html-5-autofocus-messes-up-css-loading/18945951#18945951
-  </script>
-  <script type="text/javascript" src="{{ asset('/js/jquery-3.2.1.slim.min.js') }}"></script>
-  <script type="text/javascript" src="{{ asset('/js/bootstrap.bundle.min.js') }}"></script>
-  <script type="text/javascript" src="{{ asset('/js/bootstrap.min.js') }}"></script>
-  <script type="text/javascript" src="{{ asset('/js/navbar.js') }}" defer></script>
+  <script src="{{ asset('/js/jquery-3.2.1.slim.min.js') }}"></script>
+  <script src="{{ asset('/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('/js/bootstrap.min.js') }}"></script>
+  <script src="{{ asset('/js/navbar.js') }}" defer></script>
 
 </head>
 
