b1fdd308d00b92e4c05159ac9236457912fdcb31
FernandoJAlves <up201605270@fe.up.pt>
2019-06-06 14:13:47 +0100

    Further improved notifications and documented all the notification JS

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -2,38 +2,40 @@
 
 startUp();
 
+/**
+ * Start up function that runs as the page loads
+ */
+function startUp() {
 
-function startUp(){
-
+    // Div that contains info that tells if a user is logged in or not
     let isLoggedDiv = document.querySelector('#isLogged');
 
-    console.log(isLoggedDiv.getAttribute('data-isLogged'));
-
-    if(isLoggedDiv.getAttribute('data-isLogged') == true){
+    // if the user is logged in, add logic for notification button
+    if (isLoggedDiv.getAttribute('data-isLogged') == 'true') {
         let notification_button = document.querySelector('button.dropdown-toggle');
 
         notification_button.addEventListener('click', clickNotificationAction);
-        $('.dropdown-menu').click(function(e) {
+        $('.dropdown-menu').click(function (e) {
             e.stopPropagation();
         });
     }
-
-
 }
 
-
+/**
+ *  Action that gets called after the notification button is clicked
+ */
 function clickNotificationAction() {
 
     console.log(this);
 
     // API call
     sendAjaxRequest('get', '/api/notifications', {}, clickNotificationReturn);
-
 }
 
+/**
+ * Function that gets called after the "clickNotificationAction" AJAX call returns
+ */
 function clickNotificationReturn() {
-
-    console.log(this.status);
 
     // Get and clear div
     let notifyFolder = document.querySelector('div.dropdown-menu');
@@ -45,12 +47,17 @@ function clickNotificationReturn() {
 
         let notifications = (JSON.parse(this.responseText))['notifications'];
         let projNames = (JSON.parse(this.responseText))['names'];
-        //console.log(notifications);
-        //console.log(projNames)
-        
-        for (i in notifications) {
-            console.log(notifications[i]);
-            notifyFolder.appendChild(createNotification(notifications[i], projNames[i]));
+
+        // if there are no notifications
+        if (notifications.length == 0) {
+            notifyFolder.appendChild(createEmptyNotifications());
+        }
+        else {
+            // create all the notifications
+            for (i in notifications) {
+                console.log(notifications[i]);
+                notifyFolder.appendChild(createNotification(notifications[i], projNames[i]));
+            }
         }
 
     }
@@ -58,12 +65,34 @@ function clickNotificationReturn() {
         console.log("TODO: Handle errors")
     }
 
-
-
 }
 
+/**
+ * Function to create a "no notification" element
+ */
+function createEmptyNotifications(){
+    
+    let notDiv = document.createElement('div');
+    notDiv.classList.add('notify-box', 'row', 'mx-0');
+    notDiv.setAttribute('style', 'width:100%;');
+
+    let notButton = document.createElement('a');
+    notButton.classList.add('dropdown-item', 'col-sm-10');
+    notButton.innerHTML = "No new notifications";
+    notDiv.appendChild(notButton);
+
+    return notDiv;
+}
+
+
+/**
+ * Function to create a notification element
+ * @param {*} notification 
+ * @param {*} projName 
+ */
 function createNotification(notification, projName) {
 
+    // Example notification
     //<div class="notify-box"><a class="dropdown-item" href="#">Link 1</a></div>
 
     let notDiv = document.createElement('div');
@@ -73,48 +102,49 @@ function createNotification(notification, projName) {
 
     let notButton = document.createElement('a');
     notButton.classList.add('dropdown-item', 'col-sm-10');
-    
 
     // If "is interactable"
     if (notification.interactable) {
 
+        // Add correct text
         notButton.innerHTML = "You were invited to Project '" + projName + "'";
 
+        // Create appropriate buttons
         let notAccept = createAcceptButton();
         let notDeny = createDenyButton();
 
         notAccept.addEventListener('click', readNotificationAction.bind(notDiv));
         notDeny.addEventListener('click', refuseInviteAction.bind(notDiv));
-
-
         notDiv.appendChild(notButton);
         notDiv.appendChild(notAccept);
         notDiv.appendChild(notDeny);
     }
     // If "not interactable"
     else {
-        if(notification.action == "checkPending"){
+        if (notification.action == "checkPending") {
 
+            // Add correct text and anchor
             notButton.setAttribute('href', '/projects/' + notification.id_project);
             notButton.innerHTML = "A task needs approval in Project '" + projName + "'";
-            
+
+            // Create appropriate buttons
             let notAccept = createAcceptButton();
             notAccept.addEventListener('click', readNotificationAction.bind(notDiv));
-
             notDiv.appendChild(notButton);
             notDiv.appendChild(notAccept);
         }
-        else if(notification.action == "beenRemoved"){
-            
+        else if (notification.action == "beenRemoved") {
+
+            // Add correct text
             notButton.innerHTML = "You were removed from Project '" + projName + "'";
-            
+
+            // Create appropriate buttons
             let notAccept = createAcceptButton();
             notAccept.addEventListener('click', readNotificationAction.bind(notDiv));
-
             notDiv.appendChild(notButton);
             notDiv.appendChild(notAccept);
         }
-        else{
+        else {
             // TODO: Handle de erro na base de dados?
         }
     }
@@ -122,6 +152,9 @@ function createNotification(notification, projName) {
     return notDiv;
 }
 
+/**
+ * Function to create a "Accept notification" button
+ */
 function createAcceptButton() {
 
     let notAccept = document.createElement('a');
@@ -136,6 +169,9 @@ function createAcceptButton() {
     return notAccept;
 }
 
+/**
+ * Function to create a "Refuse notification" button
+ */
 function createDenyButton() {
 
     let notDeny = document.createElement('a');
@@ -150,9 +186,11 @@ function createDenyButton() {
     return notDeny;
 }
 
+/**
+ * Action that gets called after a "Read notification" button is pressed
+ */
+function readNotificationAction() {
 
-function readNotificationAction(){
-    
     //get notification id
     let id_notify = this.getAttribute('data-id_notify');
 
@@ -163,21 +201,30 @@ function readNotificationAction(){
     this.remove();
 }
 
-function readNotificationReturn(){
-    
-    if(this.status == 200){
+/**
+ * Function that gets called after the "readNotificationAction" AJAX call returns
+ */
+function readNotificationReturn() {
+
+    if (this.status == 200) {
+        // Update the icon if necessary
         let notCount = JSON.parse(this.responseText);
-        if(notCount == 0){
+        if (notCount == 0) {
             changeNotificationIcon(false);
+            let notifyFolder = document.querySelector('div.dropdown-menu');
+            notifyFolder.appendChild(createEmptyNotifications());
         }
     }
-    else{
+    else {
         console.log("TODO: Handle errors")
     }
 
 }
 
-function refuseInviteAction(){
+/**
+ * Action that gets called after a "Refuse invite" button is pressed
+ */
+function refuseInviteAction() {
 
     //get notification id
     let id_notify = this.getAttribute('data-id_notify');
@@ -189,46 +236,55 @@ function refuseInviteAction(){
     this.remove();
 }
 
-function refuseInviteReturn(){
-    
-    if(this.status == 200){
-        // ok
+/**
+ * Function that gets called after the "refuseInviteAction" AJAX call returns
+ */
+function refuseInviteReturn() {
+
+    if (this.status == 200) {
+        // Update the icon if necessary
+
     }
-    else{
+    else {
         console.log("TODO: Handle errors");
         console.log("Status: " + status);
     }
 
 }
 
-
-function changeNotificationIcon(newVal){
+/**
+ * Function that changes the notification icon according to the argument received (true => change to "Has notifications"; false => change to "No notifications")
+ * @param {*} newVal 
+ */
+function changeNotificationIcon(newVal) {
 
     //remove old icon
     let oldIconButton = document.querySelector('#notification-icon');
 
-    while(oldIconButton.firstChild){
+    while (oldIconButton.firstChild) {
         oldIconButton.removeChild(oldIconButton.firstChild)
     }
 
     //add new icon
     let newIcon = document.createElement('img');
-    if(newVal){
+    if (newVal) {
         newIcon.src = "/icons/ban.svg";
         newIcon.alt = "There are notifications";
     }
-    else{
+    else {
         newIcon.src = "/icons/notification_center.svg";
         newIcon.alt = "No notifications";
     }
-    
+
     newIcon.classList.add('mx-1');
-    newIcon.setAttribute('style','width:45px');
-    
+    newIcon.setAttribute('style', 'width:45px');
+
     oldIconButton.appendChild(newIcon);
 }
 
 
+
+// AJAX FUNCTIONS
 
 function encodeForAjax(data) {
     if (data == null) return null;
