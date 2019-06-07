3933fe5222e7b9f4d37470e3ef6c7f0e21531d71
up201605219 <up201605219@fe.up.pt>
2019-06-07 01:46:32 +0100

    Routes added

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,12 @@ Route::post('login', 'Auth\LoginController@login');
 Route::post('register', 'Auth\RegisterController@register');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 
+// Password Reset
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
 // Projects
 
 Route::get('projects', 'ProjectsController@index');
